67505cb12bcbbbc881ecd445f8b7cd3119545967
Julian Möwenmann <moewenmann@protonmail.ch>
2024-05-30 16:39:24 +0200

    renamed, change in storing data

--- speed.py
+++ speed.py
@@ -3,7 +3,7 @@ import socket
 import datetime
 import json
 
-def get_speedtest_results():
+def run_test():
 	st = speedtest.Speedtest()
 	st.get_best_server()
 	st.download()
@@ -11,32 +11,32 @@ def get_speedtest_results():
 	results = st.results.dict()
 	return results
 
-def get_system_info():
+def sys_info():
 	hostname = socket.gethostname()
 	local_ip = socket.gethostbyname(hostname)
 	public_ip = socket.gethostbyname(socket.getfqdn())
 	return hostname, local_ip, public_ip
 
-def save_results_to_file(data, filename='speedtest_results.json'):
+def s_to_f(data, filename='data.json'):
 	with open(filename, 'a') as file:
 		json.dump(data, file, indent=4)
 
 def main():
-	results = get_speedtest_results()
-	hostname, local_ip, public_ip = get_system_info()
+	results = run_test()
+	hostname, local_ip, public_ip = sys_info()
 	current_time = datetime.datetime.now().isoformat()
 	
 	data = {
+		"TIME": current_time,
 		"ISP": results.get('client').get('isp'),
 		"IP": results.get('client').get('ip'),
 		"LOCAL_IP": local_ip,
 		"HOSTNAME": hostname,
-		"TIME": current_time,
-		"UPLOAD_SPEED": results.get('upload') / 1_000_000,
-		"DOWNLOAD_SPEED": results.get('download') / 1_000_000,
+		"UPLOAD_SPEED": round(results.get('upload') / 1_000_000, 4),
+		"DOWNLOAD_SPEED": round(results.get('download') / 1_000_000, 4),
 		"LATENCY": results.get('ping')
 	}
 	
-	save_results_to_file(data)
+	s_to_f(data)
 
 main()
